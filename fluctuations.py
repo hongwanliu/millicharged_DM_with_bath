126cb4eac0ef34ed8d607df885d867f2e78d58fe
Hongwan Liu <hongwan@mit.edu>
2022-04-05 10:27:07 -0400

    numerical calculation of correlations

--- fluctuations.py
+++ fluctuations.py
@@ -211,7 +211,7 @@ class Velocity_Fluctuations:
         float
         """
 
-        return 4 * np.pi * np.trapz(self.MB(v) * v**2 * f(v), v)
+        return 4 * np.pi * np.trapz(self.MB(v) * v**2 * f(v), v, axis=0)
 
     def var_f(self, f, v): 
         """Variance of function f over Maxwell-Boltzmann PDF. 
@@ -230,7 +230,9 @@ class Velocity_Fluctuations:
         float
         """
 
-        return 4 * np.pi * np.trapz(self.MB(v) * v**2 * (f(v) - self.mean_f(f, v))**2, v)
+        return 4 * np.pi * np.trapz(
+            self.MB(v) * v**2 * (f(v) - self.mean_f(f, v))**2, v, axis=0
+        )
 
     def bias_f(self, f, v): 
         """Bias parameter. 
@@ -254,23 +256,21 @@ class Velocity_Fluctuations:
         # Better to numerically compute than to use exact value, 29 km/s. 
         Vrms_sq = self.mean_f(lambda x:x**2, v)
 
-        print(Vrms_sq)
-        print(self.mean_f(f,v))
-        print(np.trapz(self.MB(v) * v**4 * f(v), v))
-
         if np.abs(Vrms_sq / self.sigma3D**2 - 1.) > 1e-2: 
             warnings.warn('Computed Vrms_sq is not self.sigma3D.')
 
         return 3 / 2 * (1. - (
-            4. * np.pi * np.trapz(self.MB(v) * v**4 * f(v), v) 
+            4. * np.pi * np.trapz(self.MB(v) * v**4 * f(v), v, axis=0)
             / Vrms_sq / self.mean_f(f, v)
         ))
 
 
-class Temperature_Fluctuations:
+class Fluctuations:
 
-    def __init__(self, z_ary, v_ary, Tb_ary, xA=None, sigma3D=29.): 
-        """Structure for temperature fluctuations. 
+    def __init__(self, z_ary, v_ary, f_ary, sigma3D=29.): 
+        """Structure for fluctuations. 
+
+        Although written with T21 in mind, can be extended to anything. 
 
         Parameters
         ----------
@@ -278,18 +278,147 @@ class Temperature_Fluctuations:
             Array of redshifts.
         v_ary : ndarray
             Baryon-CDM relative velocity abscissa in km/s.  
-        Tb_ary : ndarray
-            Baryon temperature in K, dimensions z_ary x v_ary. 
-        x_A : function, optional
-            Returns Lyman-alpha coupling, dimensions z_ary. If none, spin temperature is equal to baryon temperature. 
+        f_ary : ndarray
+            Function values, e.g. baryon temperature in K, dimensions v_ary x z_ary  
         sigma3D : float
             3D velocity dispersion in km/s. 
 
         Attributes
         ----------
+        v_fluc : Velocity_Fluctuations
+            Class containing velocity fluctuations information. 
+        mean : float or ndarray 
+            Mean value of f integrated over v_ary, dimensions z_ary. 
+        var : float or ndarray
+            Variance of f integrated over v_ary, dimensions z_ary. 
+        b : float or ndarray
+            Bias parameter of f, dimensions z_ary. 
+        dT2 : float or ndarray
+            <f'^2> ...
         """
 
+        self.z_ary = z_ary
+        self.v_ary = v_ary
+        self.f_ary = f_ary 
+
+        self.v_fluc = Velocity_Fluctuations(sigma3D=sigma3D)
+
+        self.f_v    = interp1d(v_ary, f_ary, axis=0)
+
+        self.mean   = self.v_fluc.mean_f(self.f_v, v_ary) 
+        self.var    = self.v_fluc.var_f(self.f_v, v_ary) 
+        self.b      = self.v_fluc.bias_f(self.f_v, v_ary)
+
+        # RETURN TO THIS, INCORRECT FOR NOW.  
+        self.dT2 = self.v_fluc.var_f(
+            interp1d(v_ary[:-1], (np.diff(f_ary) / np.diff(v_ary))**2), 
+            v_ary[:-1]
+        )
+
+    def xi_f_numerical(self, x_ary): 
+        """Numerical calculation of the correlation function. 
+
+        xi_f has units of f^2. 
+
+        Parameters
+        ----------
+        x_ary : ndarray
+            Distances in Mpc. 
+
+        Returns
+        -------
+        """
+
+        u_ary = self.v_ary / self.v_fluc.sigma1D
+        u_ary = np.linspace(u_ary[0], u_ary[-1], 70)
+        y_ary = np.linspace(-1, 1, 50)
+
+        def R(y_ary=y_ary, x_ary=x_ary): 
+            # y = cos theta
+            term_1 = self.v_fluc.cperpint(x_ary)**2 
+            term_2 = y_ary[:,None]**2 * (
+                self.v_fluc.cparint(x_ary)**2 - self.v_fluc.cperpint(x_ary)**2
+            )
+            return np.sqrt(term_1 + term_2) 
+
+        def dW(
+            y_ary=y_ary, x_ary=x_ary, u1_ary=u_ary, u2_ary=u_ary
+        ): 
+
+            one_minus_R_sq = 1. - R(y_ary, x_ary)**2 
+            u1_sq_plus_u2_sq = u1_ary[:,None]**2 + u2_ary[None,:]**2 
+
+            term_1 = np.einsum(
+                'ij,k,l->ijkl', 1./(R(y_ary, x_ary)*np.sqrt(one_minus_R_sq)), u1_ary, u2_ary
+            ) / np.pi
+
+            term_2a_exp = np.einsum(
+                'ij,kl->ijkl', 1. / (2 * one_minus_R_sq), -u1_sq_plus_u2_sq
+            )
+
+            term_2b_exp = np.einsum(
+                'ij,k,l->ijkl', 2 * R(y_ary, x_ary) / (2 * one_minus_R_sq),
+                u1_ary, u2_ary
+            )
+
+            term_2 = 0.5*np.exp(term_2a_exp + term_2b_exp)
+
+            term_3 = 1. - np.exp(-2*term_2b_exp)
+
+            dW_zero_R = np.einsum(
+                'ij,kl,k,l->ijkl',
+                np.ones_like(one_minus_R_sq),
+                np.exp(-u1_sq_plus_u2_sq/2) / np.pi, u1_ary**2, u2_ary**2
+            )
+
+            large_R_term = term_1 * term_2 * term_3 - dW_zero_R 
+
+            small_R_term = np.einsum(
+                'ij,kl,k,l->ijkl',
+                R(y_ary, x_ary)**2, 
+                np.exp(-u1_sq_plus_u2_sq / 2) / 6. / np.pi, 
+                u1_ary**2 * (u1_ary**2 - 3.), u2_ary**2 * (u2_ary**2 - 3),
+            )
+
+            mask = np.zeros_like(large_R_term) + R(y_ary, x_ary)[:,:,None,None]
+            
+            large_R_mask = np.ones_like(mask)
+            small_R_mask = np.ones_like(mask)
+            
+            large_R_mask[mask < 0.1] *= 0
+            small_R_mask[mask >= 0.1] *= 0
+            
+            return large_R_term*large_R_mask + small_R_term*small_R_mask
+
+        def dff(
+            y_ary=y_ary, x_ary=x_ary, u1_ary=u_ary, u2_ary=u_ary
+        ): 
+
+            return np.einsum(
+                'ijkl,k,l->ijkl',
+                dW(y_ary, x_ary, u1_ary, u2_ary), 
+                self.f_v(u1_ary * self.v_fluc.sigma1D),
+                self.f_v(u2_ary * self.v_fluc.sigma1D)
+            )
+
+        return np.trapz(
+            np.trapz(
+                np.trapz(
+                    dff(y_ary, x_ary, u_ary, u_ary), y_ary, axis=0
+                ),
+                u_ary, axis=-2
+            ), 
+            u_ary, axis=-1
+        )
+
+        
+
+
+
+
+
     
+
 
 
 
